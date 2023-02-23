326c167265e03f4b6fd65900a74ca1c8608d378b
Fabian Velez <fabianmoravelez6@gmail.com>
2023-02-22 22:11:03 -0500

    creando formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React,{useState} from 'react';
 import './App.css';
 import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
 
 const App = () => {
+
+  const [tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Limpiar cuarto',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Almuerzo a las 2',
+        completada: false
+      }
+    ]
+  );
+
+   console.log(tareas)
   return (
       <div className='contenedor'>
           <Header />
+          <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
       </div>
   );
 }
